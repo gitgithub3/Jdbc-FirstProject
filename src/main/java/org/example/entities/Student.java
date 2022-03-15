d96e5bef718a414453c66f202b9df3348398a766
gitgithub3 <81684104+gitgithub3@users.noreply.github.com>
2022-03-15 10:57:39 +0700

    Create class student.java
    add new entity student

---
+++ src/main/java/org/example/entities/Student.java
@@ -0,0 +1,23 @@
+public class Student{
+private int nim;
+private String name;
+  
+  
+ public void setNim(int nim){
+ this.nim=nim;
+ }
+  
+ public void setName(String name){
+ this.name=name;
+ }
+  
+ public int getNim(){
+    return nim;
+ }
+  
+  public String getName(){
+    return name;
+  }
+
+
+}
